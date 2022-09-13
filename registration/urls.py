bafc9d66906db2014644b9301dd70254a1926dfe
Devasenaganesan <98077435+Devasenaganesan@users.noreply.github.com>
2022-09-13 21:52:10 +0530

    Add files via upload

---
+++ registration/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+
+urlpatterns += [
+    url(r'^captcha/', include('captcha.urls'))
+]
---
+++ registration/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+
+from .forms import *
+
+from django.http import HttpResponse
+
+
+def success(request):
+    return HttpResponse("")
+
+# Create your views here.
+def index(request):
+    print("DEBUG! REQUEST METHOD", request.method)
+    if request.method == 'POST':
+             student_form = StudentForm(request.POST or None, request.FILES)
+             if student_form.is_valid():
+                 if student_form.cleaned_data.get('declaration'):
+                     student_form.save()
+                 else:
+                     return render(request, 'index.html', {'form': student_form, 'button': 'Submit', 'heading': 'Registration Form for Convocation 2020'})
+                 return render(request, 'success.html')
+             else:
+                 return render(request, 'index.html', {'form': student_form, 'button': 'Submit', 'heading': 'Registration Form for Convocation 2020'})
+    else:
+        print("StudentForm created")
+        student_form = StudentForm()
+        print(student_form)
+    return render(request, 'index.html', {'form': student_form, 'button': 'Submit', 'heading': 'Registration Form for Convocation 2020'})
